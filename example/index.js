1043afe1a3c615a50aac3156f0e9e7fedf9bc23c
Ionică Bizău <bizauionica@yahoo.com>
2015-05-24 14:18:40 +0300

    Example

--- example/index.js
+++ example/index.js
@@ -1,12 +1,21 @@
-var GhPolyglot = require("../lib");
+// Dependencies
+var GhPolyglot = require("../lib")
+  , Pie = require("../../cli-pie/lib")
+  ;
 
-var me = new GhPolyglot("IonicaBizau/bat");
+var me = new GhPolyglot("GhitaB/guess-my-number");
 me.repoStats(function (err, stats) {
     if (err) { return console.log(err); }
     console.log("Repo stats: ", stats);
+    console.log(new Pie(10, stats, {
+        legend: true
+    }).toString());
 });
 
 me.userStats(function (err, stats) {
     if (err) { return console.log(err); }
     console.log("Repo stats: ", stats);
+    console.log(new Pie(10, stats, {
+        legend: true
+    }).toString());
 });
