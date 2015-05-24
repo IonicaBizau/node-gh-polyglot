ce8b455fe873b516321e0f995076836f6cf0e64d
Ionică Bizău <bizauionica@yahoo.com>
2015-05-24 18:06:13 +0300

    Built the client files

---
+++ build/client.js
@@ -0,0 +1,29 @@
+// This file generates the client script
+// Dependencies
+var Fs = require("fs")
+  , GitHubColors = require("github-colors")
+  ;
+
+const TEMPLATE = "(function (root) {\n"
+               + "  var GitHubColors = __GITHUB_COLORS__;\n"
+               + "  __GITHUB_POLYGLOT__\n"
+               + "})(window);";
+
+// Configurations
+GitHubColors.init();
+
+var ghPolyglot = Fs.readFileSync(__dirname + "/../lib/index.js", "utf-8")
+  , lines = ghPolyglot.split("\n")
+  , replace = {
+        __GITHUB_POLYGLOT__: lines.slice(lines.indexOf("/**")).join("\n")
+      , __GITHUB_COLORS__: "{ get: " + GitHubColors.get.toString() + "\n"
+                         + "colors: " + JSON.stringify(GitHubColors.colors) + "}"
+    }
+  , client = TEMPLATE
+  ;
+
+Object.keys(replace).forEach(function (c) {
+    client = client.replace(new RegExp(c), replace[c]);
+});
+
+Fs.writeFileSync(__dirname + "/../dist/gh-polyglot.js", client);
